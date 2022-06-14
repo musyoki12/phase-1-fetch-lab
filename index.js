d127339550b2e8185e7c54e6e60b93a560da015c
vincenbryaniel@gmail.com <vincenbryaniel@gmail.com>
2022-06-14 22:45:08 +0300

    commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())//conversion to json
+    .then(json => renderBooks(json))// conversion to readable languange
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
